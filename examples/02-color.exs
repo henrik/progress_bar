9ca33aead55b4a082c5fe33d8ef8c78562985212
Henrik Nyh <henrik@nyh.se>
2015-09-21 21:12:55 +0200

    Another color example

--- examples/02-color.exs
+++ examples/02-color.exs
@@ -2,8 +2,23 @@ IO.puts ""
 IO.puts "Custom color:"
 
 format = [
-  bar_color: [IO.ANSI.white, IO.ANSI.green_background],
-  blank_color: IO.ANSI.red_background,
+  bar_color: [IO.ANSI.magenta],
+  blank_color: [IO.ANSI.magenta],
+  bar: "█",
+  blank: "░",
+  left: " ", right: " ",
+]
+
+Enum.each 1..100, fn (i) ->
+  ProgressBar.render(i, 100, format)
+  :timer.sleep 30
+end
+
+format = [
+  bar_color: [IO.ANSI.green_background],
+  blank_color: [IO.ANSI.red_background],
+  bar: " ",
+  blank: " ",
   left: " ", right: " ",
 ]
 
