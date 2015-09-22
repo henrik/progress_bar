c3f341096c73ecf4cb21ddf7c611cc42bb1fe614
Henrik Nyh <henrik@nyh.se>
2015-09-22 20:31:59 +0200

    Doc timer auto-cancel
    Documented here:
    http://elixir-lang.org/docs/v1.0/elixir/Process.html#send_after/3

--- lib/progress_bar/indeterminate_server.ex
+++ lib/progress_bar/indeterminate_server.ex
@@ -33,6 +33,7 @@ defmodule ProgressBar.IndeterminateServer do
   defp tick({format, count}) do
     render_bar(format, count)
 
+    # This timer is automatically cancelled when the server goes away.
     interval = format[:interval]
     Process.send_after(self, :tick, interval)
 
