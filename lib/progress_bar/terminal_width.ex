840a9ebc48286e341fbe01bfab32306db2230ba0
Henrik Nyh <henrik@nyh.se>
2015-10-01 22:55:45 +0200

    Use :io.columns to determine terminal width

--- lib/progress_bar/terminal_width.ex
+++ lib/progress_bar/terminal_width.ex
@@ -2,12 +2,9 @@ defmodule ProgressBar.TerminalWidth do
   @fallback 80
 
   def determine do
-    if System.find_executable("tput") do
-      {num_string, 0} = System.cmd("tput", ["cols"])
-      {num, _} = Integer.parse(num_string)
-      num
-    else
-      @fallback
+    case :io.columns do
+      {:ok, count} -> count
+      _ -> @fallback
     end
   end
 end
