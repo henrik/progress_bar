b4f701bf9b29ead01644863eed011e1c0d118c6c
Henrik Nyh <henrik@nyh.se>
2018-11-15 16:49:47 +0000

    Version 1.7.0
    With Elixir 1.7 support.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ProgressBar.Mixfile do
   def project do
     [
       app: :progress_bar,
-      version: "1.6.2",
+      version: "1.7.0",
       elixir: "~> 1.3",
       description: "Command-line progress bars and spinners.",
       package: package(),
