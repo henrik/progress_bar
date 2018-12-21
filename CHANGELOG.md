3ac65277a596e361634dfa39a5c9ce9bb2d336be
Henrik Nyh <henrik@nyh.se>
2018-12-21 18:50:08 +0100

    Version 2.0.0
    And have a changelog from now on.

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# 2.0.0
+
+* Replace `bytes: true` with `suffix: :bytes`, and introduce `suffix: :count`.
+
+# 1.7.1 and earlier
+
+Please see git history.
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ProgressBar.Mixfile do
   def project do
     [
       app: :progress_bar,
-      version: "1.7.1",
+      version: "2.0.0",
       elixir: "~> 1.3",
       description: "Command-line progress bars and spinners.",
       package: package(),
