2e09137cdc5f8c071d360a4da66ff324e24c446c
Pavel Makarenko <cryfall@gmail.com>
2023-08-03 18:44:31 +0400

    fixed tests

--- test/enum_test.exs
+++ test/enum_test.exs
@@ -1,24 +1,76 @@
 defmodule EnumTest do
   use ExUnit.Case
 
+  alias ProgressBar.Utils
   import ExUnit.CaptureIO
 
   test "it works with enums" do
-    capture_io(fn ->
-      list = ProgressBar.from_enum([1, 2, 3, 4, 5], fn i -> i + 1 end)
+    test_pid = self()
 
-      assert list == [1, 2, 3, 4, 5]
-    end)
+    io =
+      capture_io(fn ->
+        list = ProgressBar.from_enum([1, 2, 3, 4, 5], fn _ -> send(test_pid, :fn_run) end)
+
+        assert list == [1, 2, 3, 4, 5]
+      end)
+
+    assert split_bars(io) == [
+             "|",
+             "|",
+             "0%",
+             "|===============",
+             "|",
+             "20%",
+             "|=============================",
+             "|",
+             "40%",
+             "|============================================",
+             "|",
+             "60%",
+             "|==========================================================",
+             "|",
+             "80%",
+             "|=========================================================================|",
+             "100%"
+           ]
+
+    for _ <- 1..5, do: assert_received(:fn_run)
+    refute_received :fn_run
   end
 
   test "it works with streams" do
-    capture_io(fn ->
-      list =
-        [1, 2, 3, 4, 5]
-        |> ProgressBar.from_stream()
-        |> Enum.into([])
+    io =
+      capture_io(fn ->
+        list =
+          [1, 2, 3, 4, 5]
+          |> ProgressBar.from_stream()
+          |> Enum.into([])
 
-      assert list == [1, 2, 3, 4, 5]
-    end)
+        assert list == [1, 2, 3, 4, 5]
+      end)
+
+    assert split_bars(io) == [
+             "|",
+             "|",
+             "0%",
+             "|===============",
+             "|",
+             "20%",
+             "|=============================",
+             "|",
+             "40%",
+             "|============================================",
+             "|",
+             "60%",
+             "|==========================================================",
+             "|",
+             "80%",
+             "|=========================================================================|",
+             "100%"
+           ]
+  end
+
+  defp split_bars(string) do
+    string |> String.replace(Utils.ansi_prefix(), "\n") |> String.split()
   end
 end
